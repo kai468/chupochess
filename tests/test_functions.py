36e5562f2a64ed7ecc0fa0306d73a18b891af289
Kai L <kai.luessow@fau.de>
2022-07-25 17:04:39 +0200

    tests: added validation of attacked squares on castling path to test_King_getCastlingRights()

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -32,38 +32,63 @@ def test_King_getCastlingRights():
     # do some moves to allow kingside castling:
     # F1 -> F5
     fromSquare = board.locationSquareMap[Location(0, File.F)]
-    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.F)])
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.F)],board)
     fromSquare.reset()
     assert len(wKing.getCastlingRights(board)) == 0
     # G1 -> G5
     fromSquare = board.locationSquareMap[Location(0, File.G)]
-    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.G)])
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.G)],board)
     fromSquare.reset()
     assert len(wKing.getCastlingRights(board)) == 1
     # do some moves to allow queenside castling:
     # C1 -> C5
     fromSquare = board.locationSquareMap[Location(0, File.C)]
-    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.C)])
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.C)],board)
     fromSquare.reset()
     assert len(wKing.getCastlingRights(board)) == 1
     # D1 -> D5
     fromSquare = board.locationSquareMap[Location(0, File.D)]
-    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.D)])
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.D)],board)
     fromSquare.reset()
     assert len(wKing.getCastlingRights(board)) == 1
     # B1 -> B5
     fromSquare = board.locationSquareMap[Location(0, File.B)]
-    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.B)])
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.B)],board)
+    fromSquare.reset()
+    assert len(wKing.getCastlingRights(board)) == 2
+    # check if attacked squares are identified correctly:
+    # H8 -> C5 and C2 -> D3 to attack C1 with the rook on C5
+    fromSquare = board.locationSquareMap[Location(7, File.H)]
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.C)],board)
+    fromSquare.reset()
+    fromSquare = board.locationSquareMap[Location(1, File.C)]
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(2, File.D)],board)
+    fromSquare.reset()
+    assert len(wKing.getCastlingRights(board)) == 1
+    # H2 -> G3 and D8 -> H2 to attack F1 with the queen on H2
+    fromSquare = board.locationSquareMap[Location(1, File.H)]
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(2, File.G)],board)
+    fromSquare.reset()
+    fromSquare = board.locationSquareMap[Location(7, File.D)]
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(1, File.H)],board)
+    fromSquare.reset()
+    assert len(wKing.getCastlingRights(board)) == 0
+    # D3 -> C2 and H2 -> G3 to block/remove attackers:
+    fromSquare = board.locationSquareMap[Location(2, File.D)]
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(1, File.C)],board)
+    fromSquare.reset()
+    fromSquare = board.locationSquareMap[Location(1, File.H)]
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(2, File.G)],board)
     fromSquare.reset()
     assert len(wKing.getCastlingRights(board)) == 2
     # move kingside rook: H1->H5
     fromSquare = board.locationSquareMap[Location(0, File.H)]
-    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.H)])
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(4, File.H)],board)
     fromSquare.reset()
     assert len(wKing.getCastlingRights(board)) == 1
     # move king: E1->D1
     fromSquare = board.locationSquareMap[Location(0, File.E)]
-    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(0, File.D)])
+    fromSquare.currentPiece.makeMove(board.locationSquareMap[Location(0, File.D)],board)
     fromSquare.reset()
     assert len(wKing.getCastlingRights(board)) == 0
 
